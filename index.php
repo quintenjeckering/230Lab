f806ec0c3eabc59426dedf5e78801d9a7ea29f1a
quintenjeckering <qrj0001@mix.wvu.edu>
2021-02-04 18:27:03 -0500

    Lab2 About Page

---
+++ index.php
@@ -0,0 +1,64 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <title> Video Game Review Website </title>
+        <h1>About Us</h1>
+        <p style= "font-size: 25px;color:black">Video Game Review Team</p>
+    </div>
+    <div class = "row">
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2 style= "color:NavyBlue"> Head Critic </h2>
+                    <h3 style= "color:gray"> Quinten Jeckering </h3>
+                    <p>qjeck@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2 style= "color:gold "> Head Art Critic</h2>
+                    <h3 style= "color:gray"> Squilliam FancySon III </h3>
+                    <p>unibrow@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+           <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2 style= "color:red"> Head Action Critic </h2>
+                    <h3 style= "color:gray"> JunkRat </h3>
+                    <p>fireinthehole@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2 style= "color:violet"> Head Creativity Critic</h2>
+                    <h3 style= "color:gray"> Steve </h3>
+                    <p>ih8herobrine@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class = "description">
+        <h2 style= "color:black"> About The Critics </h2> 
+        <p> This team is composed of critics that specialize in certain aspects of Video Games. First up is Squilliam, whose wealthy lifestyle 
+            and love for art make him the perfect person for judging the graphics and art-style of games. Next we have JunkRat, whose love for explosions and action
+            is unparalleled, making him a great judge for the action contained in a game. Third we have Steve, whose reputation preceedes him. There is nothing
+            this guy can't build, making him the master of creativity. Lastly we have Quinten Jeckering, the head critic who gives the final review of games based upon
+            the input from his coworkers.
+        
+        </p>
+    
+    
+    
+    </div>
+</body>
